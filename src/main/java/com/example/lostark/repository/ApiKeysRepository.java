9951ec6a62b6f9f62fa3c24f82577a50c44c62d0
telsam <jmg97@kakao.com>
2023-02-27 07:24:48 -0800

    delete : todo

--- src/main/java/com/example/lostark/repository/ApiKeysRepository.java
+++ src/main/java/com/example/lostark/repository/ApiKeysRepository.java
@@ -1,14 +1,12 @@
 package com.example.lostark.repository;
 
 import com.example.lostark.model.entity.ApiKeys;
-import com.example.lostark.model.entity.auction.AuctionProduct;
 import feign.Param;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ApiKeysRepository extends BaseRepository<ApiKeys, Long> {
-    // Todo fix query mapping
     @Query("select a.apiKey from ApiKeys as a where a.member.uid=:uid")
     public List<String> findApiKeysByUid(@Param(value = "uid") Long uid);
 }
